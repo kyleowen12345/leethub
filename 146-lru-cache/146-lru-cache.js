b46df85973c6f46d9dbdfe16e09464da75423e43
Kyle Owen <65342160+kyleowen12345@users.noreply.github.com>
2022-05-19 06:22:07 +0800

    Time: 825 ms (17.91%), Space: 126.5 MB (28.20%) - LeetHub

---
+++ 146-lru-cache/146-lru-cache.js
@@ -0,0 +1,31 @@
+class LRUCache {
+  constructor(capacity) {
+    this.map = new Map();
+    this.capacity = capacity;
+  }
+
+  get(key) {
+    if (this.map.has(key)){
+    let val = this.map.get(key);
+    this.map.delete(key);
+    this.map.set(key, val);
+    return val;
+    }else{
+        return -1
+    }
+
+   
+  };
+
+  put(key, val) {
+      if(this.get(key) === -1) {
+          if(this.capacity === this.map.size) {
+              for(let keyVal of this.map) {
+                  this.map.delete(keyVal[0]);
+                  break;
+              }
+          }
+      }
+      this.map.set(key,val); 
+ };
+}
